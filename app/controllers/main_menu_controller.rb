33232d606761965321d7d30836c039e8cdb057cb
ayriom <ayrio@ya.ru>
2014-02-28 23:26:35 +0400

    Initial commit

---
+++ app/controllers/main_menu_controller.rb
@@ -0,0 +1,5 @@
+class MainMenuController < ApplicationController
+  def show
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hearthstone::Application.config.secret_key_base = '4104af12fc4aeb4d3190b057e89518513982bc3ea04e538659788edad4a1e0a747a8fbf32c9f7f7a114fe06c06bc395b6ed56d3ba07137703517c05586dbb72b'
